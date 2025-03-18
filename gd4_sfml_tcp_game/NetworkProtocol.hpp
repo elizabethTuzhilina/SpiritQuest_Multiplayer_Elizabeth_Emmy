f06d7125a43cfe1fee64dc275e41f23dfdb09aa7
Emmanuella-i <113349379+Emmanuella-i@users.noreply.github.com>
2025-03-18 17:21:54 +0000

    Added Network Protocol & Node Classes
    Started Network Protocol.hpp
    Started  Network Node.hpp/cpp

---
+++ gd4_sfml_tcp_game/NetworkProtocol.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <SFML/System/Vector2.hpp>
+const unsigned short SERVER_PORT = 50000;
+
+namespace Server
+{
+	//These are packets that come from the server
+	enum class PacketType
+	{
+		kBroadcastMessage,
+		kInitialState,
+		kPlayerEvent,
+		kPlayerRealtimeChange,
+		kPlayerConnect,
+		kPlayerDisconnect,
+		kAcceptCoopPartner,
+		kSpawnEnemy,
+		kSpawnPickup,
+		kSpawnSelf,
+		kUpdateClientState,
+		kMissionSuccess
+	};
+}
+
+namespace Client
+{
+	//Messages sent from the client
+	enum class PacketType
+	{
+		kPlayerEvent,
+		kPlayerRealtimeChange,
+		kRequestCoopPartner,
+		kPositionUpdate,
+		kGameEvent,
+		kQuit
+	};
+}
+
+namespace GameActions
+{
+	enum Type
+	{
+		kEnemyExplode
+	};
+
+	struct Action
+	{
+		Action() = default;
+		Action(Type type, sf::Vector2f position) :type(type), position(position)
+		{
+
+		}
+
+		Type type;
+		sf::Vector2f position;
+	};
+}
